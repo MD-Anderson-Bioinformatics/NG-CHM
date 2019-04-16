b06e464de5c4b0ddd7510ea01b081e3e93b6a37a
Bradley Broom <bmbroom@mdanderson.org>
2019-04-16 10:56:10 -0500

    Enable easy configuration of server API location
    Moves API location to a location separate from NGCHM viewer, so
    that it simplifies writing rules for front-end proxies etc.
    
    Define that location in this small static file so that it can be
    changed easily.  (e.g. by having proxy redirect to a customized file.)
    
    Also include location of linkout customization script here as well.

---
+++ NGCHM/WebContent/javascript/Config.js
@@ -0,0 +1,11 @@
+
+// Define/use Namespace for NgChm Application
+var NgChm = NgChm || { };
+NgChm.CFG = NgChm.CFG || {};
+
+// Location at which NGCHM server can be contacted.
+// Must end with a /.
+NgChm.CFG.api = 'api/';
+
+// Location of script for defining custom link outs etc.
+NgChm.CFG.custom_script = 'javascript/custom/custom.js';
