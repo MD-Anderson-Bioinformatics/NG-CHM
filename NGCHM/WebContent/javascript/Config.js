dcfcd245a801ed2fe54f10e11d2fc9329a5e6cea
mstucky <mstucky@insilico.us.com>
2019-04-25 09:49:12 -0400

    fix to Bradley's api-directory pull request

--- NGCHM/WebContent/javascript/Config.js
+++ NGCHM/WebContent/javascript/Config.js
@@ -5,7 +5,7 @@ NgChm.CFG = NgChm.CFG || {};
 
 // Location at which NGCHM server can be contacted.
 // Must end with a /.
-NgChm.CFG.api = 'api/';
+NgChm.CFG.api = '';
 
 // Location of script for defining custom link outs etc.
 NgChm.CFG.custom_script = 'javascript/custom/custom.js';
