5216e2a0a56767fca3048218d8ed2dfb7bca814d
mstucky <mstucky@insilico.us.com>
2020-06-25 11:10:17 -0400

    2.17.8 PTs: #173419205, #173420970, and #173512670

--- NGCHM/src/mda/ngchm/datagenerator/GalaxyMapGen.java
+++ NGCHM/src/mda/ngchm/datagenerator/GalaxyMapGen.java
@@ -24,7 +24,7 @@ import org.json.simple.parser.ParseException;
 public class GalaxyMapGen {
 	
 public static boolean debugOutput = false;
-private static String BUILDER_VERSION = "2.11.4";
+private static String BUILDER_VERSION = "2.11.5";
 
 
 	public static void main(String[] args){
