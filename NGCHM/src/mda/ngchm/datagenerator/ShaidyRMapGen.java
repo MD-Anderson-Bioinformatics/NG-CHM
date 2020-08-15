79068af91e97e8a091c20ced04e3f739fb8718fa
Bradley Broom <bmbroom@gmail.com>
2020-08-15 13:04:36 -0500

    Bump ShaidyR version to 2.1.1

--- NGCHM/src/mda/ngchm/datagenerator/ShaidyRMapGen.java
+++ NGCHM/src/mda/ngchm/datagenerator/ShaidyRMapGen.java
@@ -20,7 +20,7 @@ import org.json.simple.parser.ParseException;
 //various configuration / data files produced by R and stored in different directories into a
 //heatmap.json and runs HeatmapDataGenerator to create the files needed by the NGCHM viewer.
 public class ShaidyRMapGen {
-	private static String BUILDER_VER = "ShaidyR 2.1.0";
+	private static String BUILDER_VER = "ShaidyR 2.1.1";
 	private static String FILE_SEP = File.separator + File.separator; // On windows "\" path separator characters need to be doubled in json strings
 	
 	
