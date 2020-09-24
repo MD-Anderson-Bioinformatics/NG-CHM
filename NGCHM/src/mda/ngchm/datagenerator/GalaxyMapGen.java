ad0d6e03dcfa186536e0b482f849a1d415291b34
mstucky <mstucky@insilico.us.com>
2020-09-24 15:31:28 -0400

    Up version number to 2.18.3 for viewer and 2.12.2 for Galaxy

--- NGCHM/src/mda/ngchm/datagenerator/GalaxyMapGen.java
+++ NGCHM/src/mda/ngchm/datagenerator/GalaxyMapGen.java
@@ -24,7 +24,7 @@ import org.json.simple.parser.ParseException;
 public class GalaxyMapGen {
 	
 public static boolean debugOutput = false;
-private static String BUILDER_VERSION = "2.12.1";
+private static String BUILDER_VERSION = "2.12.2";
 
 
 	public static void main(String[] args){
