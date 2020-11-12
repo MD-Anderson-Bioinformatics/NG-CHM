MD-Anderson-Bioinformatics/NG-CHM

commit fea966d7d7bc74f4c2c4aa56bed8730cf81ca302

Make generalized state loss warnings more specific
Change the generalized warnings about state loss from
2a323109b6d8b6c8109091597f10f986e2af85e9 to be specific to PathwayMapper
and Scatter Plot plugins. (Otherwise the appear when the detail and
summary panes will be reset, but resetting those is not an issue for
the user because the state informatin is preserved).

NGCHM/WebContent/javascript/PaneControl.js
@@ -556,7 +556,7 @@ NgChm.Pane.ngchmContainerHeight = 100;	// Percent of window height to use for NG
 		if (!loc.pane || !loc.container) return;
 		const verticalContainer = loc.container.classList.contains('vertical');
 		// If user is attempting pane manipulation that will reset the plugin, offer them a chance to cancel:
-		if (vertical != verticalContainer) {
+		if (vertical != verticalContainer && loc.pane.textContent.indexOf('Scatter Plot') != -1) {
 			/**
 				Function to create dialog for user to choose 'Cancel' or 'OK. 
 				Returns a promise: resolve if 'OK' button clicked, reject if 'Cancel' button clicked
@@ -569,7 +569,7 @@ NgChm.Pane.ngchmContainerHeight = 100;	// Percent of window height to use for NG
 					NgChm.UHM.setMessageBoxText('This action will delete all the information in PathwayMapper. Would you like to continue?')
 				} else {
 					NgChm.UHM.setMessageBoxHeader('Pane Reset Warning');
-					NgChm.UHM.setMessageBoxText('This action will delete some settings in "'+loc.pane.textContent+'". Would you like to continue?')
+					NgChm.UHM.setMessageBoxText('This action will delete zoom and selection information in "'+loc.pane.textContent+'". Would you like to continue?')
 				}
 				NgChm.UHM.setMessageBoxButton(1, 'images/cancelSmall.png', 'Cancel Button')
 				NgChm.UHM.setMessageBoxButton(2, 'images/okButton.png', 'OK Button')
@@ -978,7 +978,7 @@ NgChm.Pane.ngchmContainerHeight = 100;	// Percent of window height to use for NG
 							NgChm.UHM.setMessageBoxText('This action will delete all information in PathwayMapper. Would you like to continue?')
 						} else {
 							NgChm.UHM.setMessageBoxHeader('Pane Reset Warning');
-							NgChm.UHM.setMessageBoxText('This action will delete some settings in "' +
+							NgChm.UHM.setMessageBoxText('This action will delete zoom and selection information in "' +
 								paneLoc.container.textContent.replace('empty','') + '". Would you like to continue?')
 						} 
 						NgChm.UHM.setMessageBoxButton(1, 'images/cancelSmall.png', 'Cancel Button')
@@ -999,7 +999,7 @@ NgChm.Pane.ngchmContainerHeight = 100;	// Percent of window height to use for NG
 						})
 					}  // end function promisePrompt
 					// If user is attempting to close a pane that will result in resetting plugin state, offer them the chance to cancel:
-					if (c.length < 4) {
+					if (c.length < 4 && paneLoc.container.textContent.indexOf('Scatter Plot') != -1) {
 						promisePrompt(paneLoc)
 							.then(function() { // promise resolved, continue pane manipulation
 								NgChm.UHM.messageBoxCancel()

commit 4b9d5b62b1ee5845db8d6325006bfcea5dc49d87

Fix bug introduced in fea966d
Commit fea966d caused the warnings to not be displayed for PathwayMapper

NGCHM/WebContent/javascript/PaneControl.js
@@ -556,7 +556,7 @@ NgChm.Pane.ngchmContainerHeight = 100;	// Percent of window height to use for NG
 		if (!loc.pane || !loc.container) return;
 		const verticalContainer = loc.container.classList.contains('vertical');
 		// If user is attempting pane manipulation that will reset the plugin, offer them a chance to cancel:
-		if (vertical != verticalContainer && loc.pane.textContent.indexOf('Scatter Plot') != -1) {
+		if (vertical != verticalContainer && (loc.pane.textContent.indexOf('Scatter Plot') != -1 || loc.pane.textContent.indexOf('PathwayMapper') > -1)) {
 			/**
 				Function to create dialog for user to choose 'Cancel' or 'OK. 
 				Returns a promise: resolve if 'OK' button clicked, reject if 'Cancel' button clicked
@@ -999,7 +999,7 @@ NgChm.Pane.ngchmContainerHeight = 100;	// Percent of window height to use for NG
 						})
 					}  // end function promisePrompt
 					// If user is attempting to close a pane that will result in resetting plugin state, offer them the chance to cancel:
-					if (c.length < 4 && paneLoc.container.textContent.indexOf('Scatter Plot') != -1) {
+					if (c.length < 4 && (paneLoc.container.textContent.indexOf('Scatter Plot') != -1 || paneLoc.container.textContent.indexOf('PathwayMapper') > -1)) {
 						promisePrompt(paneLoc)
 							.then(function() { // promise resolved, continue pane manipulation
 								NgChm.UHM.messageBoxCancel()