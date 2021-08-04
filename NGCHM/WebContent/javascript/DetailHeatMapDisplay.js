MD-Anderson-Bioinformatics/NG-CHM

commit 67abee99c3fb8018dd96a4a6bc5ff05e7d35cc10

Create Detail Map Template in JS instead of HTML
Instead of using static HTML for the detail map template, create it in
javascript. This will facilitate future work on the save state feature.

NGCHM/WebContent/javascript/DetailHeatMapDisplay.js
@@ -174,6 +174,56 @@ NgChm.DET.setInitialDetailDisplaySize = function (mapItem) {
 	}
 }
 
+/*
+	Construct DOM template for Detail Heat Map and append to div with id = 'template' 
+*/
+NgChm.DET.constructDetailMapDOMTemplate = function() {
+	let detailTemplate = document.createElement('div')
+	detailTemplate.setAttribute('id','detail_chm')
+	detailTemplate.setAttribute('class','detail_chm')
+	detailTemplate.setAttribute('style','position: absolute;')
+	let columnDendro = document.createElement('canvas')
+	columnDendro.setAttribute('id','detail_column_dendro_canvas')
+	columnDendro.setAttribute('width','1200')
+	columnDendro.setAttribute('height','500')
+	columnDendro.setAttribute('style','position: absolute;')
+	detailTemplate.appendChild(columnDendro)
+	let rowDendro = document.createElement('canvas')
+	rowDendro.setAttribute('id','detail_row_dendro_canvas')
+	rowDendro.setAttribute('width','1200')
+	rowDendro.setAttribute('height','500')
+	rowDendro.setAttribute('style','position: absolute;')
+	detailTemplate.appendChild(rowDendro)
+	let detailCanvas = document.createElement('canvas')
+	detailCanvas.setAttribute('id','detail_canvas')
+	detailCanvas.setAttribute('class','detail_canvas')
+	detailCanvas.setAttribute('tabindex','1')
+	detailTemplate.appendChild(detailCanvas)
+	let detailBoxCanvas = document.createElement('canvas')
+	detailBoxCanvas.setAttribute('id','detail_box_canvas')
+	detailBoxCanvas.setAttribute('class','detail_box_canvas')
+	detailTemplate.appendChild(detailBoxCanvas)
+	// labels div has children colLabels and rowLabels
+	let labels = document.createElement('div')
+	labels.setAttribute('id','labelDiv')
+	labels.setAttribute('style','display: inline-block;')
+	let colLabels = document.createElement('div')
+	colLabels.setAttribute('id','colLabelDiv')
+	colLabels.setAttribute('data-axis','Column')
+	colLabels.setAttribute('style','display: inline-block; position: absolute; right: 0px;')
+	colLabels.setAttribute('oncontextmenu','NgChm.DET.labelRightClick(event)')
+	labels.appendChild(colLabels)
+	let rowLabels = document.createElement('div')
+	rowLabels.setAttribute('id','rowLabelDiv')
+	rowLabels.setAttribute('data-axis','Row')
+	rowLabels.setAttribute('style','display: inline-block; position: absolute; bottom: 0px;')
+	rowLabels.setAttribute('oncontextmenu','NgChm.DET.labelRightClick(event)')
+	labels.appendChild(rowLabels)
+	detailTemplate.appendChild(labels)
+	let templates = document.getElementById('templates')
+	templates.appendChild(detailTemplate)
+}
+
 /*********************************************************************************************
  * FUNCTION:  drawDetailHeatMap - The purpose of this function is to draw the region of the 
  * NGCHM specified by drawWin to a detail heat map pane.
@@ -2360,18 +2410,18 @@ NgChm.DET.getDetFragmentShader = function (theGL) {
 	NgChm.Pane.registerPaneContentOption ('Detail heatmap', switchPaneToDetail);
 
 	let savedChmElements = [];
-	let firstSwitch = true;
 
 	function switchPaneToDetail (loc) { 
 		if (loc.pane === null) return;  //Builder logic for panels that don't show detail
 		const debug = false;
 		let isPrimary = false;
-		if (firstSwitch) {
+		let detailCanvas = document.getElementById('detail_canvas')
+		if (detailCanvas == undefined) {
 			// First time detail NGCHM created.
+			NgChm.DET.constructDetailMapDOMTemplate()
 			NgChm.SRCH.clearAllSearchResults();
 			NgChm.Pane.emptyPaneLocation (loc);
 			loc.pane.appendChild (document.getElementById('detail_chm'));
-			firstSwitch = false;
 			isPrimary = true;
 		} else {
 			NgChm.Pane.clearExistingGearDialog(loc.pane.id);

NGCHM/WebContent/chm.html
@@ -126,7 +126,7 @@
 			<div id='loader' style='display: none;'>Loading...</div>
 			<div id="ngChmContainer" class="ngChmContainer" style="position: relative"></div>
 			<div id="footer"></div>
-			<div style="display:none">
+			<div style="display:none" id='templates'>
 				<!-- Templates -->
 				<div id='summary_chm'>
 					<canvas id='column_dendro_canvas' width='1200' height='500' style="position: absolute;"></canvas>
@@ -140,16 +140,6 @@
 					<canvas id='summary_col_select_canvas' class='selection_canvas' style='padding-top:2px;z-index:0;'></canvas>
 					<canvas id='summary_row_select_canvas' class='selection_canvas' style='padding-left:2px;'></canvas>
 					<div id='sumlabelDiv' style="display: inline-block"></div>
-				</div>
-				<div id='detail_chm' class='detail_chm' style='position: relative;'>
-		 			<canvas id='detail_column_dendro_canvas' width='1200' height='500' style="position: absolute;"></canvas>
-					<canvas id='detail_row_dendro_canvas' width='1200' height='500' style="position: absolute;"></canvas>
-					<canvas id='detail_canvas' class='detail_canvas' tabindex='1'></canvas>
-					<canvas id='detail_box_canvas' class='detail_box_canvas'></canvas>
-					<div id='labelDiv' style="display: inline-block">
-						<div id='colLabelDiv' data-axis="Column" style="display: inline-block; position: absolute; right: 0px; " oncontextmenu="NgChm.DET.labelRightClick(event)"></div>
-						<div id='rowLabelDiv' data-axis ="Row" style="display: inline-block; position: absolute; bottom: 0px; " oncontextmenu="NgChm.DET.labelRightClick(event)"></div>
-					</div>
 				</div>
 			</div>
 			<div id="pdfPrefs" class="hide">

NGCHM/WebContent/javascript/NGCHM_Util.js
@@ -592,7 +592,6 @@ NgChm.UTIL.onLoadCHM = function (sizeBuilderView) {
 		}
  	} 
 	document.getElementById("summary_canvas").addEventListener('wheel', NgChm.DEV.handleScroll, NgChm.UTIL.passiveCompat({capture: false, passive: false}));
-	document.getElementById("detail_canvas").focus();
 };
 
 /**********************************************************************************

commit f8e89ebddefc0c3177eeb9daa066697c67358e77

Bug fix: closing primary detail & creating another
This commit fixes a bug introduced in 67abee99: if the user closed
the primary detail map, then tried to create additional detail maps, the
code would crash.

The fix is to introduce another quasi-global variable analogous to the
'firstSwitch' variable removed in 67abee99. This new variable is
quasi-global in order to work with the reconstruction.

NGCHM/WebContent/javascript/DetailHeatMapDisplay.js
@@ -2410,19 +2410,20 @@ NgChm.DET.getDetFragmentShader = function (theGL) {
 	NgChm.Pane.registerPaneContentOption ('Detail heatmap', switchPaneToDetail);
 
 	let savedChmElements = [];
+	NgChm.DET.initialSwitchPaneToDetail = true
 
 	function switchPaneToDetail (loc) { 
 		if (loc.pane === null) return;  //Builder logic for panels that don't show detail
 		const debug = false;
 		let isPrimary = false;
-		let detailCanvas = document.getElementById('detail_canvas')
-		if (detailCanvas == undefined) {
+		if (NgChm.DET.initialSwitchPaneToDetail == true) {
 			// First time detail NGCHM created.
 			NgChm.DET.constructDetailMapDOMTemplate()
 			NgChm.SRCH.clearAllSearchResults();
 			NgChm.Pane.emptyPaneLocation (loc);
 			loc.pane.appendChild (document.getElementById('detail_chm'));
 			isPrimary = true;
+			NgChm.DET.initialSwitchPaneToDetail = false;
 		} else {
 			NgChm.Pane.clearExistingGearDialog(loc.pane.id);
 			if (savedChmElements.length > 0) {

NGCHM/WebContent/javascript/StateManager.js
@@ -15,6 +15,7 @@ NgChm.createNS('NgChm.StateMan');
 	 */
 	async function reconstructPanelsFromMapConfig() {
 		if (NgChm.heatMap && NgChm.heatMap.isMapLoaded() && NgChm.LNK.getPanePlugins().length>0) { // map ready
+			NgChm.DET.initialSwitchPaneToDetail = true;
 			reconstructPanelLayoutFromMapConfig();
 			recreateReconstructedPanes();
 			setPanesContent()