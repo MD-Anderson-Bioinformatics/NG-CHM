317bc25018db1d8215090a874f7fd5e7ea72cb82
Mary Rohrdanz <marohrdanz@mdanderson.org>
2021-09-21 08:06:36 -0500

    Make JSHint suggestions to StateMan

--- NGCHM/WebContent/javascript/StateManager.js
+++ NGCHM/WebContent/javascript/StateManager.js
@@ -1,12 +1,12 @@
-"use strict";
+
 /**
- *	Initial stub of a file for code to handle state management. 
+ *	NGCHM namespace responsible for reconstructing a saved pane layout.
  */
 
 NgChm.createNS('NgChm.StateMan');
 
 (function(){
-
+	"use strict";
 	NgChm.StateMan.reconstructPanelsFromMapConfig = reconstructPanelsFromMapConfig;
 	NgChm.StateMan.initializePluginWithMapConfigData = initializePluginWithMapConfigData;
 
@@ -23,15 +23,15 @@ NgChm.createNS('NgChm.StateMan');
 			NgChm.DET.initialSwitchPaneToDetail = true;
 			reconstructPanelLayoutFromMapConfig();
 			recreateReconstructedPanes();
-			setPanesContent()
+			setPanesContent();
 			addDividerControlsToResizeHelpers();
 			addResizeHandlersToContainers();
-			window.dispatchEvent(new Event('resize'))
+			window.dispatchEvent(new Event('resize'));
 			triggerUpdateSelectionOnDetailMaps();
-			NgChm.SUM.summaryPaneResizeHandler()
+			NgChm.SUM.summaryPaneResizeHandler();
 			NgChm.heatMap.setUnAppliedChanges(false);
 		} else { // wait for NGCHM to initialize itself
-			setTimeout(reconstructPanelsFromMapConfig, 500)
+			setTimeout(reconstructPanelsFromMapConfig, 500);
 		}
 	}
 
@@ -46,36 +46,36 @@ NgChm.createNS('NgChm.StateMan');
 	 * TODO: fix this hack.
 	*/
 	function triggerUpdateSelectionOnDetailMaps() {
-		var idx = 0
+		var idx = 0;
 		function triggerUpdateSelection() {
 			setTimeout( () => {
 				NgChm.SEL.updateSelection(NgChm.DMM.DetailMaps[idx]);
 				idx = idx + 1;
 				if (idx < NgChm.DMM.DetailMaps.length) {
-					triggerUpdateSelection()
+					triggerUpdateSelection();
 				}
-			}, 100)
+			}, 100);
 		}
-		triggerUpdateSelection()
+		triggerUpdateSelection();
 	}
 
 	/**
 	 *	Reconstruct ngChmContainer and pane layout.
 	 */ 
 	function reconstructPanelLayoutFromMapConfig() {
-		let baseNgChmContainer = document.getElementById('ngChmContainer1')
-		let panel_layoutJSON = NgChm.heatMap.getPanelConfiguration()['panel_layout'];
-		let reconstructedPanelLayout = domJSON.toDOM(panel_layoutJSON)
-		baseNgChmContainer.parentNode.replaceChild(reconstructedPanelLayout, baseNgChmContainer)
+		let baseNgChmContainer = document.getElementById('ngChmContainer1');
+		let panel_layoutJSON = NgChm.heatMap.getPanelConfiguration().panel_layout;
+		let reconstructedPanelLayout = domJSON.toDOM(panel_layoutJSON);
+		baseNgChmContainer.parentNode.replaceChild(reconstructedPanelLayout, baseNgChmContainer);
 	}
 
 	/**
 	 * Add DividerControl methods to resizeHelper elements
 	 */
 	function addDividerControlsToResizeHelpers() {
-		let dividers = document.getElementsByClassName("resizerHelper")
+		let dividers = document.getElementsByClassName("resizerHelper");
 		for (let i=0; i<dividers.length; i++) {
-			dividers[i].dividerController = new NgChm.Pane.DividerControl(dividers[i])
+			dividers[i].dividerController = new NgChm.Pane.DividerControl(dividers[i]);
 		}
 	}
 
@@ -83,9 +83,9 @@ NgChm.createNS('NgChm.StateMan');
 	 * Add paneresize event handlers to ngChmContainer elements
 	 */
 	function addResizeHandlersToContainers() {
-		let containers = document.getElementsByClassName("ngChmContainer")
+		let containers = document.getElementsByClassName("ngChmContainer");
 		for (let i=0; i<containers.length; i++) {
-			containers[i].addEventListener('paneresize', NgChm.Pane.resizeHandler)
+			containers[i].addEventListener('paneresize', NgChm.Pane.resizeHandler);
 		}
 	}
 
@@ -95,13 +95,13 @@ NgChm.createNS('NgChm.StateMan');
 	 *	features panes should have.
 	 */
 	function recreateReconstructedPanes() {
-		NgChm.DMM.DetailMaps = []
-		let panes = document.getElementsByClassName("pane")
+		NgChm.DMM.DetailMaps = [];
+		let panes = document.getElementsByClassName("pane");
 		for (let i=0; i<panes.length; i++) {
-			let displayedPane = document.getElementById(panes[i].id)
+			let displayedPane = document.getElementById(panes[i].id);
 			let newPane = NgChm.Pane.newPane({height: displayedPane.clientHeight+"px", width: displayedPane.clientWidth+"px"}, displayedPane.textContent,
-				displayedPane.id)
-			displayedPane.parentNode.replaceChild(newPane,displayedPane)
+				displayedPane.id);
+			displayedPane.parentNode.replaceChild(newPane,displayedPane);
 		}
 	}
 
@@ -111,30 +111,30 @@ NgChm.createNS('NgChm.StateMan');
 	 *	primary detail pane first. TODO: clean this up!)
 	 */
 	function setPanesContent() {
-		NgChm.DMM.nextMapNumber = 1
-		setPrimaryDetailPaneContent()
-		let panes = document.getElementsByClassName("pane")
-		let highestDetailMapNumber = 1 
+		NgChm.DMM.nextMapNumber = 1;
+		setPrimaryDetailPaneContent();
+		let panes = document.getElementsByClassName("pane");
+		let highestDetailMapNumber = 1;
 		for (let i=0; i<panes.length; i++) {
-			setPaneContentUnlessPrimaryDetail(panes[i].id)
-			if (NgChm.DMM.nextMapNumber > highestDetailMapNumber) { highestDetailMapNumber = NgChm.DMM.nextMapNumber }
+			setPaneContentUnlessPrimaryDetail(panes[i].id);
+			if (NgChm.DMM.nextMapNumber > highestDetailMapNumber) { highestDetailMapNumber = NgChm.DMM.nextMapNumber; }
 		}
-		let highestPaneId = getHighestPaneId()
+		let highestPaneId = getHighestPaneId();
 		NgChm.Pane.resetPaneCounter(highestPaneId + 1);
-		NgChm.DMM.nextMapNumber = highestDetailMapNumber
+		NgChm.DMM.nextMapNumber = highestDetailMapNumber;
 	}
 
 	/**
 		Return hightest pane id for panes currently in DOM
 	*/
 	function getHighestPaneId() {
-		let panes = document.getElementsByClassName("pane")
+		let panes = document.getElementsByClassName("pane");
 		try {
-			let paneIds = [...panes].map(p => {return p.id}).map(p => {return parseInt(p.replace('pane',''))})
-			return Math.max(...paneIds)
+			let paneIds = [...panes].map(p => {return p.id;}).map(p => {return parseInt(p.replace('pane',''));});
+			return Math.max(...paneIds);
 		} catch(err) {
-			console.error("Cannot determine hightest pane id")
-			throw err
+			console.error("Cannot determine hightest pane id");
+			throw err;
 		}
 	}
 
@@ -143,14 +143,14 @@ NgChm.createNS('NgChm.StateMan');
 	 * return null.
 	 */
 	function getPrimaryDetailPane() {
-		let panes = document.getElementsByClassName("pane")
+		let panes = document.getElementsByClassName("pane");
 		for (let i=0; i<panes.length; i++) {
-			let pane = document.getElementById(panes[i].id)
+			let pane = document.getElementById(panes[i].id);
 			if (pane.textContent.includes("Heat Map Detail - Primary")) {
-				return pane
+				return pane;
 			}
 		}
-		return null
+		return null;
 	}
 
 	/**
@@ -158,15 +158,15 @@ NgChm.createNS('NgChm.StateMan');
 	 */
 	function setPrimaryDetailPaneContent() {
 		let pane = getPrimaryDetailPane();
-		NgChm.DMM.primaryMap.pane = pane.id
-		NgChm.DET.switchPaneToDetail(NgChm.Pane.findPaneLocation(pane))
-		let canvas = pane.querySelector('.detail_canvas')
-		let mapItem = NgChm.DMM.getMapItemFromCanvas(canvas)
-		let chm = document.getElementById('detail_chm')
-		NgChm.DMM.completeMapItemConfig(chm, mapItem)
-		NgChm.DET.setDrawDetailTimeout(mapItem,5,false)
-		NgChm.DET.drawRowAndColLabels(mapItem)
-		NgChm.DEV.addEvents(pane.getAttribute('id'))
+		NgChm.DMM.primaryMap.pane = pane.id;
+		NgChm.DET.switchPaneToDetail(NgChm.Pane.findPaneLocation(pane));
+		let canvas = pane.querySelector('.detail_canvas');
+		let mapItem = NgChm.DMM.getMapItemFromCanvas(canvas);
+		let chm = document.getElementById('detail_chm');
+		NgChm.DMM.completeMapItemConfig(chm, mapItem);
+		NgChm.DET.setDrawDetailTimeout(mapItem,5,false);
+		NgChm.DET.drawRowAndColLabels(mapItem);
+		NgChm.DEV.addEvents(pane.getAttribute('id'));
 		NgChm.DMM.switchToPrimary(pane.childNodes[1]);
 		NgChm.DET.updateDisplayedLabels();
 	}
@@ -176,27 +176,27 @@ NgChm.createNS('NgChm.StateMan');
 	 *	return)
 	 */
 	function setPaneContentUnlessPrimaryDetail(paneid) {
-		let pane = document.getElementById(paneid)
+		let pane = document.getElementById(paneid);
 		let customjsPlugins = NgChm.LNK.getPanePlugins(); // plugins from custom.js
 		if (pane.textContent.includes("Heat Map Summary")) {
-			NgChm.SUM.switchPaneToSummary(NgChm.Pane.findPaneLocation(pane))
+			NgChm.SUM.switchPaneToSummary(NgChm.Pane.findPaneLocation(pane));
 		} else if (pane.textContent.includes("Heat Map Detail - Primary")) {
-			return
+			return;
 		} else if (pane.textContent.includes("Heat Map Detail")) {
-			NgChm.DMM.nextMapNumber = parseInt(pane.textContent.split('Ver ')[1]) - 1
-			let loc = NgChm.Pane.findPaneLocation(pane)
-			NgChm.DET.switchPaneToDetail(NgChm.Pane.findPaneLocation(pane))
+			NgChm.DMM.nextMapNumber = parseInt(pane.textContent.split('Ver ')[1]) - 1;
+			let loc = NgChm.Pane.findPaneLocation(pane);
+			NgChm.DET.switchPaneToDetail(NgChm.Pane.findPaneLocation(pane));
 		} else if (pane.textContent == 'empty') {
-			return //empty pane
+			return;
 		} else {
 			try {
-				let specifiedPlugin = customjsPlugins.filter(pc => pane.textContent.indexOf(pc.name) > -1)[0]
-				NgChm.LNK.switchPaneToPlugin(NgChm.Pane.findPaneLocation(pane),specifiedPlugin)
-				NgChm.Pane.initializeGearIconMenu(document.getElementById(paneid+'Icon'))
+				let specifiedPlugin = customjsPlugins.filter(pc => pane.textContent.indexOf(pc.name) > -1)[0];
+				NgChm.LNK.switchPaneToPlugin(NgChm.Pane.findPaneLocation(pane),specifiedPlugin);
+				NgChm.Pane.initializeGearIconMenu(document.getElementById(paneid+'Icon'));
 			} catch(err) {
-				console.error(err)
-				console.error("Specified plugin: ",pane.textContent)
-				throw("Error loading plugin")
+				console.error(err);
+				console.error("Specified plugin: ",pane.textContent);
+				throw("Error loading plugin");
 			}
 		}
 	}
@@ -205,38 +205,38 @@ NgChm.createNS('NgChm.StateMan');
 	* Send any exiting data from mapConfig.json to plugin and close Gear Menu
 	*/
 	function initializePluginWithMapConfigData(pluginInstance) {
-		let paneId = getPaneIdFromInstance(pluginInstance)
-		let pluginConfigData = getPluginDataFromMapConfig(paneId)
+		let paneId = getPaneIdFromInstance(pluginInstance);
+		let pluginConfigData = getPluginDataFromMapConfig(paneId);
 		if (pluginConfigData) {
 			let nonce = pluginInstance.nonce;
 			let config = pluginConfigData.config;
 			let data = pluginConfigData.data;
-			NgChm.LNK.sendMessageToPlugin({nonce, op: 'plot', config, data})
-			let dataFromPlugin = pluginConfigData.dataFromPlugin
-			NgChm.LNK.sendMessageToPlugin({nonce, op: 'savedPluginData', dataFromPlugin})
-			NgChm.Pane.removePopupNearIcon(document.getElementById(paneId+'Gear'), document.getElementById(paneId+'Icon'))
+			NgChm.LNK.sendMessageToPlugin({nonce, op: 'plot', config, data});
+			let dataFromPlugin = pluginConfigData.dataFromPlugin;
+			NgChm.LNK.sendMessageToPlugin({nonce, op: 'savedPluginData', dataFromPlugin});
+			NgChm.Pane.removePopupNearIcon(document.getElementById(paneId+'Gear'), document.getElementById(paneId+'Icon'));
 		} else {
 			return false;
 		}
 	}
 
 	function getPaneIdFromInstance(pluginInstance) {
-		let paneId = pluginInstance.iframe.parentElement.parentElement.id
+		let paneId = pluginInstance.iframe.parentElement.parentElement.id;
 		if (paneId == null) {
-			throw "No pane found for this plugin"
+			throw "No pane found for this plugin";
 		}
 		return paneId;
 	}
 
 	function getPluginDataFromMapConfig(paneId) {
 		try {
-			let pluginConfigData = NgChm.heatMap.getPanelConfiguration()[paneId]
+			let pluginConfigData = NgChm.heatMap.getPanelConfiguration()[paneId];
 			return pluginConfigData;
 		} catch (error) {
 			if (error instanceof TypeError) {
-				return false
+				return false;
 			} else {
-				throw error
+				throw error;
 			}
 		}
 	}
