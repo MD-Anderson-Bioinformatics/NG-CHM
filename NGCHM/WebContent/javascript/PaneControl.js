f8717ebd81da6a3628ec19dc221aa69e9fdf5c9d
Mary Rohrdanz <marohrdanz@mdanderson.org>
2022-04-12 20:28:59 -0500

    Keep pdfpref, message, & linkout dialogs in view
    Similar to the user preferences dialog (a2dc3506), we want to keep these
    additional elements in the viewport as the user resizes the window.

--- NGCHM/WebContent/javascript/PaneControl.js
+++ NGCHM/WebContent/javascript/PaneControl.js
@@ -273,6 +273,9 @@ NgChm.Pane.ngchmContainerHeight = 100;	// Percent of window height to use for NG
 				}
 				updatePopupPositions ();
 				NgChm.UPM.keepElementInViewport("prefs");
+				NgChm.UPM.keepElementInViewport("pdfPrefs");
+				NgChm.UPM.keepElementInViewport("msgBox");
+				NgChm.UPM.keepElementInViewport("linkBox");
 			}
 		}
 	}
